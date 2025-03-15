a22b64ec37df4315ca6c337ee0b6779fb380db61
CARLOS ALEXANDRE ANDRADE DOS ANJOS DA SILVA <carlos.aaasilva@senacsp.edu.br>
2025-03-14 21:59:02 -0300

    Aula 14-03

--- frontend/script.js
+++ frontend/script.js
@@ -1,91 +1,86 @@
-// const { resolve } = require("path");
-
+// Ao campo CEP perder o foco será feito a tratativa
 document.getElementById("cep").addEventListener("blur", async function () {
   const cep = this.value.replace(/\D/g, ""); // Remove caracteres não numericos
 
-  //Verifica se o cep tem exatamente 8 Dig
+  // Verifica se o CEP tem exatamente 8 Dig.
   if (cep.length !== 8) {
-    alert("cep inválido, deve ter 8 digitos"); //Alerta se for inválido
-    return; //Não  executar o código para baixo
+    alert("CEP inválido, deve ter 8 digitos"); // Alerta se for inválido
+    return; // Não executa o código para baixo
   }
 
   try {
-    //Faz uma requisição para o backend e a consulta do CEP informado
+    // Faz uma requisição para o Backend e a consulta do CEP informado
     const response = await fetch(`http://localhost:3000/api/cep/${cep}`);
     if (!response.ok) {
-      //verifica se a resposta foi bem sucedida
-      throw new Error("Erro ao buscar o cep"); //Erro ao falhar
+      // Verifica se a resposta foi bem sucedida
+      throw new Error("Erro ao buscar o CEP"); // Erro ao falhar
     }
 
-    // Converte  a resposta da req. para JSON
+    // Converte a resposta da req. para JSON
     const data = await response.json();
 
-    //Verifica se o CEp retornado pela Api é inválido
+    // Verifica se o CEP retornado pela API é inválido
     if (data.erro) {
       alert("Cep não encontrado!");
       return;
     }
 
-    //Prenche os campos do formulário com dados retornados
+    // Preenche os campos do formulário com os dados retornardos
     document.getElementById("logradouro").value = data.logradouro;
     document.getElementById("bairro").value = data.bairro;
     document.getElementById("cidade").value = data.localidade;
     document.getElementById("estado").value = data.uf;
 
-    //Adiciona um fedback visual, alterando a cor da borda dos campos
-    document.querySelectorAll(".form-group imput").forEach((imput) => {
-      Input.style.bordercolor = "#6a11cb"; //Borda roxa ao CPF encontrado
+    // Adiciona um feedback visual, alterando a cor da borda dos campos
+    document.querySelectorAll(".form-group input").forEach((input) => {
+      input.style.borderColor = "#6a11cb"; // Borda roxa ao CPF for encontrado
     });
   } catch (error) {
-    console.erro("Erro ao buscar o cep:", error); //Exibi o erro do console
-    alert("Erro ao buscar i cep. Verifique o console para mais detalhes");
+    console.error("Erro ao buscar o CEP:", error); // Exibe o erro no console
+    alert("Erro ao buscar o CEP. Verifique o console para mais detalhes");
   }
 });
 
+// Adiciona um evento de envio
 document
   .getElementById("addressForm")
   .addEventListener("submit", async function (e) {
-    e.preventDefault(); //imprede o recarregamento da pagina ao enviar o formulario
- 
-    //obtem os valores dos campos do formularios e armazena
- 
+    e.preventDefault(); // Impede o recarregamento da página ao enviar o formulario
+
+    // Obtém os valores dos campos do formulário e armazena
     const cep = document.getElementById("cep").value;
     const logradouro = document.getElementById("logradouro").value;
     const bairro = document.getElementById("bairro").value;
     const cidade = document.getElementById("cidade").value;
     const estado = document.getElementById("estado").value;
+
     try {
-      // faz a requisição POST para o backend para salvar o endereço
-      const response = await fetch("http://localhost:3000/api/adress", {
+      // Faz a requisição POST para o backend para salvar o endereço
+      const response = await fetch("http://localhost:3000/api/address", {
         method: "POST",
         headers: {
-          "Conten-Type": "application/json", // Define o envio do conteudo como JSON
+          "Content-Type": "application/json", // Define o envio do conteudo como JSON
         },
         body: JSON.stringify({ cep, logradouro, bairro, cidade, estado }), // Envia os campos
       });
- 
-      if (!response.ok) {
-        // verifica se a resposta foi bem-sucedida
-        throw new error("Erro ao salvar o Endereçço!"); // Retorna um erro se falhar
-      }
- 
-      // Converte a resposta req. em JSON
-      const result = await response.json();
-      alert(result.message); // Exibe a mesnagem de sucesso retornada pelo backend
-  
-      // Limpa os campos do formulario após o envio bem-sucedido
-      document.getElementById("Addressform").reset();
 
-      //Remove o feadback visual(borda colorida)
-      document.querySelectorAll(".form-group input").forEach((input) => {
-        input.style.bordercolor = "#ddd"; // Define a borda de vilta para o padrão
+      if (!response.ok) {
+        // Verifica se a resposta foi bem-sucedida
+        throw new Error("Erro ao salvar o endereço!"); // Retorna um erro se falhar
+      }
+
+      // Converte a resposta req. para JSON
+      const result = await response.json();
+      alert(result.message); // Exibe a mensagem de sucesso retornada pelo Backend
+
+      // Limpa os campos do formuçário após o envio bem-sucedido
+      document.getElementByAll(".form-group input").forEach((input) => {
+        input.style.borderColor = "#ddd"; // Define a borda de volta para o padrão
       });
     } catch (error) {
       console.error("Erro ao salvar o endereço", error);
       alert(
         "Erro ao salvar o endereço. Verifique o console para mais detalhes!."
-      )
+      );
     }
   });
-
-
